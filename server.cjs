f499dc75b80ea7aaa35b05cf3692cd40f9829f6c
madhunish <madhunisha.se21@bitsathy.ac.in>
2024-02-13 15:21:55 +0530

    essage

--- server.cjs
+++ server.cjs
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const {connectToDb, getDb} = require('./dbConnection.cjs')
 const {ObjectId} = require('mongodb')
 const app = express()
+app.use(cors())
 app.use(bodyParser.json());
 
 let db
